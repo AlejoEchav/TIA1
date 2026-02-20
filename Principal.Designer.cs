2d64bcc2951b054e6d55e046a8d87931dca314bc
AlejoEchav <alejoechavarria123@gmail.com>
2026-02-20 10:24:22 -0500

    Agregar archivos de proyecto.

---
+++ Principal.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace TIA1
+{
+    partial class Principal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            archivoToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            tablasToolStripMenuItem = new ToolStripMenuItem();
+            facturacionToolStripMenuItem = new ToolStripMenuItem();
+            seguridadToolStripMenuItem = new ToolStripMenuItem();
+            ayudaToolStripMenuItem = new ToolStripMenuItem();
+            formularioClientesToolStripMenuItem = new ToolStripMenuItem();
+            formularioProductoToolStripMenuItem = new ToolStripMenuItem();
+            formularioCategoriasToolStripMenuItem = new ToolStripMenuItem();
+            formularioFacturasToolStripMenuItem = new ToolStripMenuItem();
+            formularioInformesToolStripMenuItem = new ToolStripMenuItem();
+            empleadosToolStripMenuItem = new ToolStripMenuItem();
+            rolesToolStripMenuItem = new ToolStripMenuItem();
+            seguridadToolStripMenuItem1 = new ToolStripMenuItem();
+            ayudaToolStripMenuItem1 = new ToolStripMenuItem();
+            acercaDeToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { archivoToolStripMenuItem, tablasToolStripMenuItem, facturacionToolStripMenuItem, seguridadToolStripMenuItem, ayudaToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            archivoToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { salirToolStripMenuItem });
+            archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            archivoToolStripMenuItem.Size = new Size(60, 20);
+            archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(180, 22);
+            salirToolStripMenuItem.Text = "Salir";
+            salirToolStripMenuItem.Click += salirToolStripMenuItem_Click;
+            // 
+            // tablasToolStripMenuItem
+            // 
+            tablasToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { formularioClientesToolStripMenuItem, formularioProductoToolStripMenuItem, formularioCategoriasToolStripMenuItem });
+            tablasToolStripMenuItem.Name = "tablasToolStripMenuItem";
+            tablasToolStripMenuItem.Size = new Size(52, 20);
+            tablasToolStripMenuItem.Text = "Tablas";
+            // 
+            // facturacionToolStripMenuItem
+            // 
+            facturacionToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { formularioFacturasToolStripMenuItem, formularioInformesToolStripMenuItem });
+            facturacionToolStripMenuItem.Name = "facturacionToolStripMenuItem";
+            facturacionToolStripMenuItem.Size = new Size(81, 20);
+            facturacionToolStripMenuItem.Text = "Facturacion";
+            // 
+            // seguridadToolStripMenuItem
+            // 
+            seguridadToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { empleadosToolStripMenuItem, rolesToolStripMenuItem, seguridadToolStripMenuItem1 });
+            seguridadToolStripMenuItem.Name = "seguridadToolStripMenuItem";
+            seguridadToolStripMenuItem.Size = new Size(72, 20);
+            seguridadToolStripMenuItem.Text = "Seguridad";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            ayudaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { ayudaToolStripMenuItem1, acercaDeToolStripMenuItem });
+            ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            ayudaToolStripMenuItem.Size = new Size(53, 20);
+            ayudaToolStripMenuItem.Text = "Ayuda";
+            // 
+            // formularioClientesToolStripMenuItem
+            // 
+            formularioClientesToolStripMenuItem.Name = "formularioClientesToolStripMenuItem";
+            formularioClientesToolStripMenuItem.Size = new Size(180, 22);
+            formularioClientesToolStripMenuItem.Text = "Clientes";
+            formularioClientesToolStripMenuItem.Click += formularioClientesToolStripMenuItem_Click;
+            // 
+            // formularioProductoToolStripMenuItem
+            // 
+            formularioProductoToolStripMenuItem.Name = "formularioProductoToolStripMenuItem";
+            formularioProductoToolStripMenuItem.Size = new Size(130, 22);
+            formularioProductoToolStripMenuItem.Text = "Productos";
+            // 
+            // formularioCategoriasToolStripMenuItem
+            // 
+            formularioCategoriasToolStripMenuItem.Name = "formularioCategoriasToolStripMenuItem";
+            formularioCategoriasToolStripMenuItem.Size = new Size(180, 22);
+            formularioCategoriasToolStripMenuItem.Text = "Categorias";
+            // 
+            // formularioFacturasToolStripMenuItem
+            // 
+            formularioFacturasToolStripMenuItem.Name = "formularioFacturasToolStripMenuItem";
+            formularioFacturasToolStripMenuItem.Size = new Size(180, 22);
+            formularioFacturasToolStripMenuItem.Text = "Facturas";
+            // 
+            // formularioInformesToolStripMenuItem
+            // 
+            formularioInformesToolStripMenuItem.Name = "formularioInformesToolStripMenuItem";
+            formularioInformesToolStripMenuItem.Size = new Size(180, 22);
+            formularioInformesToolStripMenuItem.Text = "Informes";
+            // 
+            // empleadosToolStripMenuItem
+            // 
+            empleadosToolStripMenuItem.Name = "empleadosToolStripMenuItem";
+            empleadosToolStripMenuItem.Size = new Size(180, 22);
+            empleadosToolStripMenuItem.Text = "Empleados";
+            // 
+            // rolesToolStripMenuItem
+            // 
+            rolesToolStripMenuItem.Name = "rolesToolStripMenuItem";
+            rolesToolStripMenuItem.Size = new Size(180, 22);
+            rolesToolStripMenuItem.Text = "Roles";
+            // 
+            // seguridadToolStripMenuItem1
+            // 
+            seguridadToolStripMenuItem1.Name = "seguridadToolStripMenuItem1";
+            seguridadToolStripMenuItem1.Size = new Size(180, 22);
+            seguridadToolStripMenuItem1.Text = "Seguridad";
+            // 
+            // ayudaToolStripMenuItem1
+            // 
+            ayudaToolStripMenuItem1.Name = "ayudaToolStripMenuItem1";
+            ayudaToolStripMenuItem1.Size = new Size(180, 22);
+            ayudaToolStripMenuItem1.Text = "Ayuda";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            acercaDeToolStripMenuItem.Size = new Size(180, 22);
+            acercaDeToolStripMenuItem.Text = "Acerca De";
+            // 
+            // Principal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            IsMdiContainer = true;
+            MainMenuStrip = menuStrip1;
+            Name = "Principal";
+            Text = "Principal";
+            WindowState = FormWindowState.Maximized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem archivoToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem tablasToolStripMenuItem;
+        private ToolStripMenuItem formularioClientesToolStripMenuItem;
+        private ToolStripMenuItem formularioProductoToolStripMenuItem;
+        private ToolStripMenuItem formularioCategoriasToolStripMenuItem;
+        private ToolStripMenuItem facturacionToolStripMenuItem;
+        private ToolStripMenuItem formularioFacturasToolStripMenuItem;
+        private ToolStripMenuItem formularioInformesToolStripMenuItem;
+        private ToolStripMenuItem seguridadToolStripMenuItem;
+        private ToolStripMenuItem ayudaToolStripMenuItem;
+        private ToolStripMenuItem empleadosToolStripMenuItem;
+        private ToolStripMenuItem rolesToolStripMenuItem;
+        private ToolStripMenuItem seguridadToolStripMenuItem1;
+        private ToolStripMenuItem ayudaToolStripMenuItem1;
+        private ToolStripMenuItem acercaDeToolStripMenuItem;
+    }
+}
---
+++ Principal.cs
@@ -0,0 +1,20 @@
+namespace TIA1
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void formularioClientesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
